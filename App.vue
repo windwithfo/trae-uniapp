a6fef9fc8500c93c141a13ed4cf7544fb487c2f5
dongkunshan <windwithfo@yeah.net>
2025-11-03 19:09:56 +0800

    1106578993591177

--- App.vue
+++ App.vue
@@ -2,12 +2,96 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			
+			// 全局路由守卫
+			uni.addInterceptor('navigateTo', {
+				invoke: (args) => {
+					return this.checkLogin(args.url)
+				}
+			})
+			
+			uni.addInterceptor('redirectTo', {
+				invoke: (args) => {
+					return this.checkLogin(args.url)
+				}
+			})
+			
+			uni.addInterceptor('switchTab', {
+				invoke: (args) => {
+					return this.checkLogin(args.url)
+				}
+			})
+			
+			uni.addInterceptor('reLaunch', {
+				invoke: (args) => {
+					return this.checkLogin(args.url)
+				}
+			})
+			
+			uni.addInterceptor('navigateBack', {
+				invoke: (args) => {
+					// 获取当前页面栈
+					const pages = getCurrentPages()
+					if (pages.length > 0) {
+						const currentPage = pages[pages.length - 1]
+						return this.checkLogin(currentPage.route)
+					}
+					return true
+				}
+			})
 		},
 		onShow: function() {
 			console.log('App Show')
 		},
 		onHide: function() {
 			console.log('App Hide')
+		},
+		methods: {
+			// 检查登录状态
+			checkLogin(url) {
+				console.log('checkLogin', url)
+				// 登录页面不需要检查
+				if (url.includes('/pages/login/login')) {
+					return true
+				}
+				
+				// 检查是否已登录
+				const isLoggedIn = this.$store.getters.isLoggedIn
+				console.log('isLoggedIn', isLoggedIn)
+				if (!isLoggedIn) {
+					uni.redirectTo({
+						url: '/pages/login/login'
+					})
+					uni.showToast({
+						title: '请先登录',
+						icon: 'none',
+						duration: 2000
+					})
+					return false
+				}
+				
+				// 检查登录是否过期
+				const loginTime = this.$store.getters.getLoginTime
+				if (loginTime) {
+					const now = Date.now()
+					const elapsed = now - loginTime
+					if (elapsed > 120000) {
+						// 登录已过期，清除用户信息
+						this.$store.commit('clearUserInfo')
+						uni.redirectTo({
+							url: '/pages/login/login'
+						})
+						uni.showToast({
+							title: '登录已过期，请重新登录',
+							icon: 'none',
+							duration: 2000
+						})
+						return false
+					}
+				}
+				
+				return true
+			}
 		}
 	}
 </script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,7 +3,11 @@
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
+			<text class="username" v-if="userInfo">欢迎, {{userInfo.username}}</text>
 		</view>
+		<button class="logout-btn" v-if="userInfo" @click="handleLogout">
+			登出
+		</button>
 	</view>
 </template>
 
@@ -11,14 +15,23 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: '首页',
+				userInfo: null
 			}
 		},
 		onLoad() {
-
+			// 获取用户信息
+			this.userInfo = this.$store.getters.getUserInfo
+		},
+		onShow() {
+			// 页面显示时更新用户信息
+			this.userInfo = this.$store.getters.getUserInfo
 		},
 		methods: {
-
+			// 登出
+			handleLogout() {
+				this.$store.dispatch('logout')
+			}
 		}
 	}
 </script>
@@ -48,5 +61,26 @@
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+
+	.username {
+		font-size: 32rpx;
+		color: #1890ff;
+		margin-top: 20rpx;
+	}
+
+	.logout-btn {
+		width: 200rpx;
+		height: 60rpx;
+		background-color: #ff4d4f;
+		color: white;
+		border: none;
+		border-radius: 30rpx;
+		margin-top: 40rpx;
+		font-size: 28rpx;
+	}
+
+	.logout-btn:active {
+		opacity: 0.8;
 	}
 </style>
